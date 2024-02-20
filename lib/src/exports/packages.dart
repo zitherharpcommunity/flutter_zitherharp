f61815e3648e03d194e2f0ce5f3784dff4f49db8
chunglhb <chunglhb@intern.vn>
2024-02-20 16:17:47 +0700

    add: firebase dependencies

--- lib/src/exports/packages.dart
+++ lib/src/exports/packages.dart
@@ -1,4 +1,5 @@
 library packages;
 
 export '../packages/exports/flutter_bloc.dart';
+export '../packages/exports/flutter_fire.dart';
 export '../packages/exports/url_launcher.dart';
---
+++ lib/src/packages/exports/flutter_fire.dart
@@ -0,0 +1,6 @@
+export 'package:firebase_analytics/firebase_analytics.dart';
+export 'package:firebase_app_check/firebase_app_check.dart';
+export 'package:firebase_crashlytics/firebase_crashlytics.dart';
+export 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+export 'package:firebase_remote_config/firebase_remote_config.dart';
+export 'package:firebase_storage/firebase_storage.dart';

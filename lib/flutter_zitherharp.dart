7e5241f2b0a33e5693183d20cbb82a3d4b29e078
lehoangbaochung <lehoangbaochung@gmail.com>
2024-03-29 23:08:49 +0700

    export packages

--- lib/flutter_zitherharp.dart
+++ lib/flutter_zitherharp.dart
@@ -3,12 +3,15 @@ library flutter_zitherharp;
 export 'dart:async';
 export 'dart:convert';
 
+export 'package:cached_network_image/cached_network_image.dart';
+export 'package:feedback/feedback.dart';
 export 'package:flutter/foundation.dart';
 export 'package:flutter/gestures.dart';
 export 'package:flutter/material.dart';
-export 'package:flutter/scheduler.dart' hide Flow;
+export 'package:flutter/scheduler.dart' hide Flow, timeDilation;
 export 'package:flutter/services.dart';
 export 'package:intl/intl.dart' hide TextDirection;
+export 'package:quick_actions/quick_actions.dart';
 
 export '/src/exports/constants.dart';
 export '/src/exports/entities.dart';

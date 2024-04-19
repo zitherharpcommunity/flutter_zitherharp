fd6f0b62cc712acb543c86cec0f72db4add5ee3e
chunglhb <chunglhb@intern.vn>
2024-04-19 17:28:58 +0700

    add: listener for cubit_state

--- lib/src/packages/flutter_bloc/cubit_state.dart
+++ lib/src/packages/flutter_bloc/cubit_state.dart
@@ -2,36 +2,7 @@ import 'package:flutter_zitherharp/flutter_zitherharp.dart';
 
 /// The logic and internal state for a [BaseCubit], [BaseState] and [StatefulWidget].
 abstract base class CubitState<C extends BaseCubit<S>, S extends BaseState,
-    W extends StatefulWidget> extends State<W> {
-  /// Whether to listen the stream of type [C].
-  bool get listen => true;
-
-  /// Obtain a value from the nearest ancestor provider of type [C].
-  @protected
-  C get cubit => context.read<C>();
-
-  /// Obtain a value from the nearest ancestor provider of type [C],
-  /// and subscribe to the provider.
-  @protected
-  S get state => context.watch<C>().state;
-
-  /// The current [stream] of [state].
-  @protected
-  Stream<S> get stream => context.watch<C>().stream;
-
-  /// Describes the state of the user interface represented by [W].
-  Widget buildState();
-
-  /// Describes the part of the user interface represented by [W].
-  @override
-  Widget build(BuildContext context) {
-    if (!listen) return buildState();
-    return CubitBuilder<C, S>(
-      cubit: cubit,
-      builder: (_, __, ___) => buildState(),
-    );
-  }
-}
+    W extends StatefulWidget> extends State<W> with CubitStateMixin<C, S, W> {}
 
 /// The logic and internal state mixin for a [BaseCubit], [BaseState] and [StatefulWidget].
 base mixin CubitStateMixin<C extends BaseCubit<S>, S extends BaseState,
@@ -55,6 +26,10 @@ base mixin CubitStateMixin<C extends BaseCubit<S>, S extends BaseState,
   /// Describes the state of the user interface represented by [W].
   Widget buildState();
 
+  /// Takes the [BuildContext] along with the [cubit] state
+  /// and is responsible for executing in response to state changes.
+  void listenState() {}
+
   /// Describes the part of the user interface represented by [W].
   @override
   Widget build(BuildContext context) {
@@ -62,6 +37,7 @@ base mixin CubitStateMixin<C extends BaseCubit<S>, S extends BaseState,
     return CubitBuilder<C, S>(
       cubit: cubit,
       builder: (_, __, ___) => buildState(),
+      listener: (_, __, ___) => listenState(),
     );
   }
 }

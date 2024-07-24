e466f3064e03d0e1522649638ad3f60573473605
chunglhb <chunglhb@intern.vn>
2024-07-24 15:32:30 +0700

    update export

--- lib/src/exports/widgets.dart
+++ lib/src/exports/widgets.dart
@@ -2,5 +2,6 @@ export '../widgets/action_button.dart';
 export '../widgets/async_builder.dart';
 export '../widgets/basic/margin.dart';
 export '../widgets/colored_tabbar.dart';
+export '../widgets/flutter_app.dart';
 export '../widgets/image_factory.dart';
 export '../widgets/text_builder.dart';
